6f81c5527029ec1c42fd395d767f8473c7698709
bsofcs <42695433+bsofcs@users.noreply.github.com>
2020-06-26 00:29:11 +0530

    Create allocatePagesInLibrary.py

---
+++ allocatePagesInLibrary.py
@@ -0,0 +1,34 @@
+def checkIfPossible(A,B,n,mid):
+ noOfStudent=1
+ curSum=0
+ for i in range(n):
+  if A[i]>mid:
+   return False
+  if curSum+A[i]>mid:
+   noOfStudent+=1
+   curSum=A[i]
+   if noOfStudent>B:
+    return False
+  else:
+   curSum+=A[i]
+ return True
+
+def allocatePagesInLibrary(A,B):
+ n=len(A)
+ if n not in range(1,10**5+1) or any(x not in range(1,10**5+1) for x in A) or n<B:
+  return None
+ high,low,result=0,0,float("INF")
+ for i in A:
+  high+=i
+ while(low<=high):
+  mid=low+(high-low)//2
+  if checkIfPossible(A,B,n,mid):
+   high=mid-1
+   result=min(result,mid)
+  else:
+   low=mid+1
+ return result
+
+A = [12, 34, 67, 90]
+B = 2
+print(allocatePagesInLibrary(A,B))
