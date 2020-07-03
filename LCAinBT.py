693874c04d759aace590db989261f16a0665cb84
bsofcs <42695433+bsofcs@users.noreply.github.com>
2020-07-03 23:59:08 +0530

    Update LCAinBT.py

--- LCAinBT.py
+++ LCAinBT.py
@@ -1,11 +1,28 @@
-def LeftView(root):
- if root is None:
+class Node:
+ def __init__(self,val):
+  self.left=None
+  self.right=None
+  self.data=val
+
+
+def LCA(root,p,q):
+ if root is None or p is None or q is None:
   return
- print(root.data, end=" ")
- if root.left is not None:
-  LeftView(root.left)
- elif root.left is None and root.right is not None:
-  LeftView(root.right)
- else:
-  return None
-  
+ if root.data==p or root.data==q:
+  return root
+ lh=LCA(root.left,p,q)
+ rh=LCA(root.right,p,q)
+ if lh and rh:
+  return root
+ return (lh if lh else rh)
+ 
+
+
+root=Node(4)
+root.right=Node(5)
+root.right.right=Node(6)
+root.left=Node(2)
+root.left.left=Node(1)
+root.left.right=Node(3)
+p=1;q=3
+print(LCA(root,p,q).data)
